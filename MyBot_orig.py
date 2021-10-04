e43929a6e90f1c02cb859612068811c85333ca5b
Neil Aspin <neilaspin@clara.co.uk>
2021-10-04 10:24:26 +0100

    Ants are now moving

--- MyBot_orig.py
+++ MyBot_orig.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from ants import *
 
+
 # define a class with a do_turn method
 # the Ants.run method will parse and update bot input
 # it will also run the do_turn method for us
@@ -8,56 +9,47 @@ class MyBot:
     def __init__(self):
         # define class level variables, will be remembered between turns
         pass
-    
+
     # do_setup is run once at the start of the game
     # after the bot has received the game settings
     # the ants class is created and setup by the Ants.run method
     def do_setup(self, ants):
         # initialize data structures after learning the game settings
         pass
-    
+
     # do turn is run once per turn
     # the ants class has the game state and is updated by the Ants.run method
     # it also has several helper methods to use
     def do_turn(self, ants):
-        # track all moves, prevent collisions
-        orders = {}  # Dictionary of all orders issued
-        targets = {}  # Dictioanary of all food targets
-
-        def do_move_direction(loc, direction):
-            new_loc = ants.destination(loc, direction)
-            if (ants.unoccupied(new_loc) and new_loc not in orders):
-                ants.issue_order((loc, direction))
-                orders[new_loc] = loc
-                return True
-            else:
-                return False
-
+        # loop through all my ants and try to give them orders
+        # the ant_loc is an ant location tuple in (row, col) form
         for ant_loc in ants.my_ants():
+            # try all directions in given order
             directions = ('n', 'e', 's', 'w')
             for direction in directions:
-                if do_move_direction(ant_loc, direction):
+                # the destination method will wrap around the map properly
+                # and give us a new (row, col) tuple
+                new_loc = ants.destination(ant_loc, direction)
+                # passable returns true if the location is land
+                if (ants.passable(new_loc)):
+                    # an order is the location of a current ant and a direction
+                    ants.issue_order((ant_loc, direction))
+                    # stop now, don't give 1 ant multiple orders
                     break
+            # check if we still have time left to calculate more orders
+            if ants.time_remaining() < 10:
+                break
 
-        # def do_move_location(loc, dest):
-        #     directions = ants.direction(loc, dest)
-        #     for direction in directions:
-        #         if do_move_direction(loc, direction):
-        #             targets[dest] = loc
-        #             return True
-        #     return False
 
-            # default move
-
-            
 if __name__ == '__main__':
     # psyco will speed up python a little, but is not needed
     try:
         import psyco
+
         psyco.full()
     except ImportError:
         pass
-    
+
     try:
         # if run is passed a class with a do_turn method, it will do the work
         # this is not needed, in which case you will need to write your own
